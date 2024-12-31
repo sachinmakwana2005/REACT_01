b61cfa0031f3674e8af7f3d986330405c0c5adfc
HARSHAD PARMAR <harshadparmar9000@gmail.com>
2024-12-31 10:34:30 +0530

    add file

--- react-project/src/componets/functioncomponent/Propsdemoinfun.jsx
+++ react-project/src/componets/functioncomponent/Propsdemoinfun.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
       //RAFC
-export const Propsdemoinfun = () => {
+export const Propsdemoinfun = (props) => {
   return (
     <div>
         <h1>Propsdemoinfun</h1>
+        <h2>{props.username}</h2>
+        <h2>{props.address}</h2>
     </div>
   )
 } 
